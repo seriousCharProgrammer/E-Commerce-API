fb6fd370ccb5b28917a71ac32cd57873565a8747
seriousCharProgrammer <charbelalhajj81@gmail.com>
2024-11-19 09:33:15 +0200

    the Order func Done

--- Controllers/OrderController.js
+++ Controllers/OrderController.js
@@ -5,6 +5,7 @@ const asyncHandler = require("express-async-handler");
 const Cart = require("../models/CartModel");
 const { get } = require("../Routes/orderRoute");
 const { getAllProducts } = require("./ProductController");
+const ErrorResponse = require("../utils/ErrorResponse");
 exports.getAllOrders = asyncHandler(async (req, res, next) => {
   const orders = await Order.findAll({ where: { user_id: req.user.id } });
 
@@ -29,28 +30,33 @@ exports.createOrder = asyncHandler(async (req, res, next) => {
   if (cartItems.length === 0) {
     return res.status(400).json({ message: "Cart is empty" });
   }
-  const products = cartItems.map((el) => el.product_id);
-  let prices = [];
+  let total = 0;
+  let prodArr = [];
 
-  const leo = products.map(async (el) => {
-    const s = await Product.findByPk(el);
-    el = s.price;
-  });
-  console.log(products);
-  console.log(prices);
-  console.log(leo);
+  for (let i = 0; i < cartItems.length; i++) {
+    console.log(cartItems[i].product_id);
+    const product = await Product.findByPk(cartItems[i].product_id);
+    prodArr.push(product.id);
+    if (!product || product.stock < cartItems[i].quantity) {
+      throw new Error(
+        `Product ${product?.name || cartItems[i].productId} is out of stock`
+      );
+    }
+    total = Math.ceil(total + product.price * cartItems[i].quantity);
+    product.stock = product.stock - cartItems[i].quantity;
+    await product.save();
+  }
 
-  /*
   const newOrder = await Order.create({
     user_id: req.user.id,
-    total_price: prices,
+    total_price: total,
     status: "pending",
-    product_id: products,
+    product_id: prodArr,
   });
 
   // Clear the cart
   await Cart.destroy({ where: { user_id: req.user.id } });
-*/
+
   res.status(200).json({
     success: true,
     data: newOrder,
